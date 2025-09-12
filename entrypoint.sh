cb46badceba60b7eed9779516a9f02a5f9ceae65
ulcheyev <ulcheyev@fel.cvut.cz>
2025-09-12 08:36:38 +0200

    [Fix] missmatched template name

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Substitute env vars into template
 echo "🛠️ Rendering consul.hcl from template..."
-envsubst < /config/consul.hcl.template.template > /config/consul.hcl.template
+envsubst < /config/consul.hcl.template > /config/consul.hcl.template
 
 echo "🚀 Starting Consul agent..."
 exec consul agent -config-file=/config/consul.hcl.template
